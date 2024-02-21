a7359e6415f1323d9b45d3229dff8f14527201b3
Arun <prakaasharun50@gmail.com>
2024-02-22 01:25:36 +0530

    text streaming beta

--- src/openvoice_streaming_server/api.py
+++ src/openvoice_streaming_server/api.py
@@ -6,8 +6,3 @@ app = FastAPI()
 
 routers = synthesize.router
 app.include_router(routers, prefix="/v1")
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, port=8000, reload=True)
--- src/openvoice_streaming_server/routers/synthesize.py
+++ src/openvoice_streaming_server/routers/synthesize.py
@@ -1,34 +1,60 @@
 import json
 
 import torch
-from fastapi import WebSocket, APIRouter
+from fastapi import APIRouter, WebSocket
 from starlette.websockets import WebSocketDisconnect
 
 from openvoice_streaming_server.openvoice_stream import StreamingBaseSpeakerTTS
 
 router = APIRouter()
 
-en_checkpoint_base = "checkpoints/base_speakers/EN"
+
+async def send_audio_stream(websocket: WebSocket, audio_stream):
+    async for audio_chunk in audio_stream:
+        await websocket.send_bytes(audio_chunk)
+
+
+class WebSocketHandler:
+    def __init__(self, tts_model):
+        self.model = tts_model
+        self.connections = set()
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.add(websocket)
+
+    async def disconnect(self, websocket: WebSocket):
+        self.connections.remove(websocket)
+
+    async def handle_websocket(self, websocket: WebSocket):
+        await self.connect(websocket)
+        try:
+            while True:
+                data = await websocket.receive_text()
+                data = json.loads(data)
+                text = data.get("text")
+                speaker = data.get('speaker', 'default')
+                language = data.get('language', 'english')
+                speed = data.get('speed', 1.0)
+                audio_stream = self.model.tts_stream(text, speaker, language, speed)
+                await send_audio_stream(websocket, audio_stream)
+        except WebSocketDisconnect:
+            await self.disconnect(websocket)
+        except Exception as e:
+            print(f"Error during text synthesis: {e}")
+            await self.disconnect(websocket)
+
+
+en_checkpoint_base = "../checkpoints/base_speakers/EN"
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 # Load models and resources
-en_base_speaker_tts = StreamingBaseSpeakerTTS(f'{en_checkpoint_base}/config.json', device=device)
-model = en_base_speaker_tts.load_ckpt(f'{en_checkpoint_base}/checkpoint.pth')  # Initialize your TTS class instance here
+model = StreamingBaseSpeakerTTS(f'{en_checkpoint_base}/config.json', device=device)
+model.load_ckpt(f'{en_checkpoint_base}/checkpoint.pth')
+
+handler = WebSocketHandler(model)
 
 
 @router.websocket("/synthesize")
 async def synthesize(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        try:
-            data = await websocket.receive_text()
-            data = json.loads(data)
-            text = data['text']
-            speaker = data.get('speaker')
-            language = data.get('language')
-            speed = data.get('speed', 1.0)
-            audio_stream = model.tts_stream(text, speaker, language, speed)
-            async for audio_chunk in audio_stream:
-                await websocket.send_bytes(audio_chunk)
-        except WebSocketDisconnect:
-            break
+    await handler.handle_websocket(websocket)
