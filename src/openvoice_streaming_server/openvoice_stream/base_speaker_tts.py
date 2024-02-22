85aae883f21c48002acce16f752d3171cfc3d0ca
Arun <prakaasharun50@gmail.com>
2024-02-22 12:47:15 +0530

    test_server for llm streaming, openvoice streaming

--- src/openvoice_streaming_server/openvoice_stream/base_speaker_tts.py
+++ src/openvoice_streaming_server/openvoice_stream/base_speaker_tts.py
@@ -1,5 +1,4 @@
 import re
-
 import torch
 
 from openvoice.api import BaseSpeakerTTS
@@ -11,23 +10,26 @@ class StreamingBaseSpeakerTTS(BaseSpeakerTTS):
         "chinese": "ZH",
     }
 
-    async def generate_audio_chunks(self, text_chunks, speaker, language='English', speed=1.0):
+    async def generate_audio_chunks(self, text, speaker, language='English', speed=1.0):
         mark = self.language_marks.get(language.lower(), None)
         assert mark is not None, f"language {language} is not supported"
 
-        t = re.sub(r'([a-z])([A-Z])', r'\1 \2', text_chunks)
-        t = f'[{mark}]{t}[{mark}]'
-        stn_tst = self.get_text(t, self.hps, False)
-        device = self.device
-        speaker_id = self.hps.speakers[speaker]
-        with torch.no_grad():
-            x_tst = stn_tst.unsqueeze(0).to(device)
-            x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).to(device)
-            sid = torch.LongTensor([speaker_id]).to(device)
-            audio = self.model.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=0.667, noise_scale_w=0.6,
-                                     length_scale=1.0 / speed)[0][0, 0].data.cpu().float().numpy()
-            yield audio
+        texts = self.split_sentences_into_pieces(text, mark)
 
-    async def tts_stream(self, text_chunks, speaker, language='English', speed=1.0):
-        async for audio_chunk in self.generate_audio_chunks(text_chunks, speaker, language, speed):
+        for t in texts:
+            t = re.sub(r'([a-z])([A-Z])', r'\1 \2', t)
+            t = f'[{mark}]{t}[{mark}]'
+            stn_tst = self.get_text(t, self.hps, False)
+            device = self.device
+            speaker_id = self.hps.speakers[speaker]
+            with torch.no_grad():
+                x_tst = stn_tst.unsqueeze(0).to(device)
+                x_tst_lengths = torch.LongTensor([stn_tst.size(0)]).to(device)
+                sid = torch.LongTensor([speaker_id]).to(device)
+                audio = self.model.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=0.667, noise_scale_w=0.6,
+                                         length_scale=1.0 / speed)[0][0, 0].data.cpu().float().numpy()
+                yield audio
+
+    async def tts_stream(self, text, speaker, language='English', speed=1.0):
+        async for audio_chunk in self.generate_audio_chunks(text, speaker, language, speed):
             yield audio_chunk.tobytes()
